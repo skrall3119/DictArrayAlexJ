5facfb153efdc30abe7a6a3c4aa6a80a55c26399
skrall3119 <alexjanci@yahoo.com>
2020-03-04 14:03:30 -0600

    init

--- DictArrayAlexJ/main.swift
+++ DictArrayAlexJ/main.swift
@@ -8,5 +8,24 @@
 
 import Foundation
 
-print("Hello, World!")
+let clothes: [String:Int] = ["Shirts": 32, "Socks": 30, "Pants": 10, "Jackets": 4]
 
+let shirtColors: [String:Int] = ["Red": 4, "Black": 10, "Blue": 5, "Green": 8, "White": 5]
+let sockColors = ["Black", "White"]
+
+let sortedClothes = clothes.sorted(by: {$0 > $1})
+let sortedShirts = shirtColors.sorted(by: {$0 > $1})
+
+for (key, value) in clothes{
+    print("Currently in my closet and/or dresser, there are \(value) \(key)")
+}
+
+for (key, value) in sortedClothes{
+    print("Here's the same stuff but sorted :D \(value) \(key)")
+}
+
+for (key, value) in sortedShirts{
+    print("Out of my \(clothes["Shirts"] ?? 0) shirts, \(value) are \(key)")
+}
+
+print("I dont have colorful socks so I only have \(sockColors[0]) and \(sockColors[1])")
